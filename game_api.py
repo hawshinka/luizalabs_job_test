54cdc8b0bac505b828902639091cadcc1d7822c1
Yauari Vieira <yvieira@ciandt.com>
2021-06-04 14:38:23 -0300

    Improve response code and exception handling

--- game_api.py
+++ game_api.py
@@ -1,5 +1,6 @@
 import flask
 import json
+from flask import Response
 from log_parser import LogParser
 
 app = flask.Flask(__name__)
@@ -9,15 +10,17 @@ app.config["DEBUG"] = False
 @app.route('/', defaults={'id': None}, methods=['GET'])
 @app.route('/<id>', methods=['GET'])
 def game_list(id):
-    log_parser = LogParser()
-    result = log_parser.parse_log("challenge/games.log")
+    try:
+        log_parser = LogParser()
+        result = log_parser.parse_log("challenge/games.log")
 
-    tag = f"game_{id}"
-    if id and result.get(tag, None):
-        return {tag: result[tag]}
+        tag = f"game_{id}"
+        if id and result.get(tag, None):
+            return {tag: result[tag]}
 
-    return json.dumps(result)
-
+        return Response(json.dumps(result), status=200, mimetype='application/json')
+    except FileNotFoundError:
+        return Response('{"Error": "Log file not found"}', status=404, mimetype='application/json')
 
 if __name__ == "__main__":
     app.run()
