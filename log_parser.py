42f082a56eead5d4789f5c9a398bb74a3cd1bf60
Yauari Vieira <yvieira@ciandt.com>
2021-06-04 14:29:47 -0300

    Fix in RE

--- log_parser.py
+++ log_parser.py
@@ -63,7 +63,7 @@ class LogParser:
         """
         try:
             pattern = re.compile(r"""
-                                 ^\s{1,2}\d*:\d*\s  # " 13:37 "
+                                 ^\s*\d*:\d*\s  # " 13:37 "
                                  \w{21}             # "ClientUserinfoChanged"    
                                  :\s*               # ": "               
                                  \d*\s*\w\\         # "2 n\" 
